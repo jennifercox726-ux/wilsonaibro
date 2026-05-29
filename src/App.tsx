c32f8a9382aeb80a65caca31c217c8e2bf3afc8d
jennifercox726-ux <jennifercox726@gmail.com>
2026-05-29 16:04:49 -0500

    Add CommandCentral import from WilsonCore

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { supabase } from "@/integrations/supabase/client";
+import CommandCentral from './WilsonCore';
 import Index from "./pages/Index.tsx";
 import Auth from "./pages/Auth.tsx";
 import NotFound from "./pages/NotFound.tsx";
